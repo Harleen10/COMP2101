b2d0b58f13a7d0ee1abebf6dcdc55ffc553c3c1e
harleen <harleen1996dillon@gmail.com>
2019-09-27 12:22:25 -0700

    final diceroller.sh

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -4,7 +4,17 @@
 #
 
 # Improve this script by adding a second line of output showing a roll of 5 six-sided dice
+echo "
+Rolling 5 six-sided dice...
+Rolling...
+$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )) rolled
+"
 # Improve this script by adding a third line of output that rolls a single 20-sided die
+echo "
+Rolling 1 twenty-sided dice...
+Rolling...
+$(( RANDOM % 20 + 1)) rolled
+"
 # These improvements should also take into consideration the user, and make all 3 output lines self-explanatory to the user
 #    - e.g. the first line should be changed to read something like:
 #         Rolling a pair of standard dice: x,y rolled
@@ -15,16 +25,4 @@ echo "
 Rolling a pair of standard dice...
 Rolling...
 $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1 )) rolled
-"
-
-echo "
-Rolling 5 six-sided dice...
-Rolling...
-$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )), $(( RANDOM % 6 + 1 )) rolled
-"
-
-echo "
-Rolling 1 twenty-sided dice...
-Rolling...
-$(( RANDOM % 20 + 1)) rolled
 "
